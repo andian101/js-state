49f82d468db569df193d9996711e81414b33d9fb
Ian <ian.moss@and.digital>
2021-01-15 18:03:53 +0000

    Updated todoReducer

--- src/store/reducers/todoReducer.js
+++ src/store/reducers/todoReducer.js
@@ -1,7 +1,7 @@
 // Group Action names
-const ADD = 'ADD';
-const TOGGLE = 'TOGGLE';
-const DELETE = 'DELETE';
+const ADD_TODO = 'ADD_TODO';
+const TOGGLE_TODO_COMPLETE = 'TOGGLE_TODO_COMPLETE';
+const DELETE_TODO = 'DELETE_TODO';
 
 // Initial data
 const initialState = [
@@ -11,31 +11,50 @@ const initialState = [
         description: "Wash bike. Its is dirty",
         completed: false
     }
-]
+];
+
+const createTodo = (toDo) => {
+    return {
+        type: ADD_TODO,
+        payload: toDo
+    }
+}
+
+const toggleComplete = (id) => {
+    return {
+        type: TOGGLE_TODO_COMPLETE,
+        payload: id
+    }
+}
+
+const deleteTodo = (id) => {
+    return {
+        type: DELETE_TODO,
+        payload: id
+    }
+}
 
 // Our reducer
 const todoReducer = (state, action) => {
     switch(action.type){ 
-        case ADD:
+        case ADD_TODO:
             return [
                 ...state,
                 {
                     ...action.payload
                 }
             ]
-        case TOGGLE: 
+        case TOGGLE_TODO_COMPLETE: 
             return state.map(note => (
                 note.id === action.payload.id ? 
                     {...note, completed: !note.completed} : 
                     note
             ))
         
-        case DELETE: 
+        case DELETE_TODO: 
             return state.filter(note => note.id !== action.payload.id);
         
         default: 
             return state;
     }
-    
-
 }
